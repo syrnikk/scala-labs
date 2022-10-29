a0478755433ccfd67130b6df00cc7d7ef7f02744
Kacper Syrnik <kacper.syrnik1@gmail.com>
2022-10-29 20:22:15 +0200

    Add lab1

---
+++ lab1/scala/Exercise2.scala
@@ -0,0 +1,9 @@
+import scala.math.sqrt
+
+object Exercise2 {
+  def main(args: Array[String]): Unit = {
+    val n = if (args.length > 0) args(0).toInt else 50;
+    val numbers = for i <- 1 to n if !sqrt(i).isWhole yield i;
+    println(numbers.toArray.mkString(" "));
+  }
+}
